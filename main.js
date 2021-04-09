9fe513167f9a7093ad3155b3aca4d4484356983e
judge2010 <74088083+judge2010@users.noreply.github.com>
2021-04-09 15:47:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 function preload(){
 
 };
-function draw(){
-
-};
 function setup(){
-Canvas = createCanvas(400 ,400);
-Canvas.position(600 ,200)
+    video=createCapture(VIDEO);
+    video.size(500 ,500);
+Canvas = createCanvas(500 ,500);
+Canvas.position(530 ,150);
+poseNet=ml5.poseNet(video,modelLoaded);
+poseNet.on("pose",gotPoses);
+};
+function draw(){
+background("#0cf21f")
+};
+function modelLoaded(){
+    console.log("Model is loaded!");
+    };
+function gotPoses(results){
+console.log(results);
 };
